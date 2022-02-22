1da48363631d9f82b219e72511519a56e3ffc408
Daniel Gyoerffy <49782833+Dansoftowner@users.noreply.github.com>
2022-02-22 20:06:27 +0100

    preparations

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ultrabalaton
+{
+    class Player
+    {
+
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Category { get; set; }
+        public DateTime AccomplishedTime { get; set; }
+        public int FinishRate { get; set; }
+
+        public Player(string name, int number, string category, DateTime accomplishedTime, int finishRate)
+        {
+            Name = name;
+            Number = number;
+            Category = category;
+            AccomplishedTime = accomplishedTime;
+            FinishRate = finishRate;
+        }
+    }
+}
---
+++ Players.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Ultrabalaton
+{
+    class Players
+    {
+
+        private List<Player> PlayerList;
+
+        public Players(List<Player> playerList)
+        {
+            PlayerList = playerList;
+        }
+
+        public Players(string filePath) : this(
+            File.ReadLines(filePath)
+                .Skip(1)
+                .Select(it => it.Split(";"))
+                .Select(it => new Player(
+                    it[0],
+                    int.Parse(it[1]),
+                    it[2],
+                    DateTime.ParseExact(it[3], "hh:mm:ss", CultureInfo.InvariantCulture),
+                    int.Parse(it[4]))
+                ).ToList()
+        )
+        {
+
+        }
+    }
+}
